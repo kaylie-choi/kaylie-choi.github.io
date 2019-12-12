33b83f6437098bed5a17ac654a1ec4e25c79bb54
Kaylie Choi <k33choi@uwaterloo.ca>
2019-12-12 16:28:00 -0500

    Remove test contact form
    php is not supported by github pages as it is only static website hosting

--- js/main.js
+++ js/main.js
@@ -43,11 +43,14 @@ $(document).ready(function(){
 
 //$('body').scrollspy({target:'.navbar', offset:50});
 
+/*
 $(document).ready(function(){
 	$('[data-spy="scroll"]').each(function () {
 		var $spy = $(this).scrollspy('refresh')
   });
-});
+});*/
+
+
 
 // fade in and out scroll to top button
 $(document).ready(function(){
@@ -85,51 +88,3 @@ if (typeof console === 'object') {
 		'\n'
 	);
 }
-
-
-// php
-$(function () {
-
-    // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
-
-    $('#contact-form').validator();
-
-
-    // when the form is submitted
-    $('#contact-form').on('submit', function (e) {
-
-        // if the validator does not prevent form submit
-        if (!e.isDefaultPrevented()) {
-            var url = "contact.php";
-
-            // POST values in the background the the script URL
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    // data = JSON object that contact.php returns
-
-                    // we recieve the type of the message: success x danger and apply it to the
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
-                        $('#contact-form')[0].reset();
-                    }
-                }
-            });
-            return false;
-        }
-    })
-});
